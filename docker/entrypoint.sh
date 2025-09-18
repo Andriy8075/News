5aa24f8283371b4bb418d3ac9b707ec2cd003774
andriy8075 <andriy8075@gmail.com>
2025-09-18 18:03:11 +0300

    entrypoint config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,8 @@
 if [ ! -f /var/www/docker/.first_run_done ]; then
   echo "First run detected – running setup commands..."
   # Run commands only on first build
-  cd /www/var/backend
+  touch whereAmI.txt
+  cd /var/www/backend || exit 1
   composer install
   cp .env.example .env
   php artisan key:generate
