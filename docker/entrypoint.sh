865cb3b29ce0c50149d9d614bdaf48ba8f081027
andriy8075 <andriy8075@gmail.com>
2025-09-18 18:13:18 +0300

    entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,15 +3,18 @@
 if [ ! -f /var/www/docker/.first_run_done ]; then
   echo "First run detected – running setup commands..."
   # Run commands only on first build
-  cd /www/var/backend
+  cd /var/www/backend || exit 1
   composer install
   cp .env.example .env
   php artisan key:generate
   php artisan storage:link
-  npm install --prefix frontend
+  cd ../frontend || exit 1
+  npm install
+  cd ..
   touch /var/www/docker/.first_run_done
 fi
 
 echo "Starting servers..."
 php backend/artisan serve --host=0.0.0.0 --port=8000 &
-npm run dev --prefix frontend
+cd frontend &&
+npm run dev frontend
