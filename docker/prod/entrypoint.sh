a75dbfe24a9aafc284510a3da0bfe89e99177671
andriy8075 <andriy8075@gmail.com>
2025-11-26 13:44:54 +0200

    optimize views, routes and use cache in production

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -1,5 +1,10 @@
 php artisan key:generate
-php artisan storage:link
 php artisan migrate
+php artisan storage:link
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+php artisan optimize:clear
+php artisan optimize
 
 php artisan serve --host=0.0.0.0 --port=8060
